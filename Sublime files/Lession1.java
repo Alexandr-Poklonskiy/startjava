226500cd49f211fd8d465755f7f77ff896c962b9
Alexandr-Poklonskiy <poklonskiy.a@gmail.com>
2022-04-01 14:24:52 +0300

    Мой первый коммит! Бинго!

---
+++ Sublime files/Lession1.java
@@ -0,0 +1,30 @@
+public class MyFirstApp {   
+    public static void main(String[] args) {
+
+
+        System.out.println("Max numbers");
+
+        byte byteMax = 127;
+        short shortMax = 32767;
+        int intMax = 2147483647;
+        long longMax = 9223372036854775807L;
+
+        System.out.println(byteMax);
+        System.out.println(shortMax);
+        System.out.println(intMax);
+        System.out.println(longMax);
+
+        System.out.println("Inc");
+
+        byte byteInc = (byte) (byteMax + 1);
+        short shortInc = (short) (shortMax + 1);
+        int intInc = intMax + 1;
+        long longInc = longMax + 1;
+
+        System.out.println(byteInc);
+        System.out.println(shortInc);
+        System.out.println(intInc);
+        System.out.println(longInc);
+        
+    }
+}
