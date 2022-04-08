09ba06f5fcb66f5ba19d1474531e577fa71ee9bf
Alexandr-Poklonskiy <poklonskiy.a@gmail.com>
2022-04-08 19:32:26 +0300

    Исправил по циклам что смог

--- Lession1/CyclesTheme.java
+++ Lession1/CyclesTheme.java
@@ -1,5 +1,3 @@
-import java.text.DecimalFormat;
-
 public class CyclesTheme {
     public static void main(String[] args) {
         // 1. Подсчет суммы четных и нечетных чисел.
@@ -11,7 +9,7 @@ public class CyclesTheme {
         do {
             if(num1 % 2 == 0) {
                 sumEven += num1;
-            }else{
+            } else {
                 sumOdd += num1;
             }
             num1++;
@@ -23,14 +21,12 @@ public class CyclesTheme {
         num1 = 10;
         num2 = 5;
         int num3 = -1;
-        int maxN = 10;
-        int minN = 5;
+        int maxN = num1;
+        int minN = num2;
         if(num1 > num2) {
             if(num1 > num3) {
                 if(num2 > num3) {
                     minN = num3;
-                } else {
-                minN = num2;
                 }
             }
         } else if(num2 > num3) {
@@ -45,53 +41,58 @@ public class CyclesTheme {
             minN = num1;
         }
 
-        for(int i = ++minN; i < maxN; i++) {
+        for(int i = --maxN; i > minN; i--) {
             System.out.print(i + " ");
         }
 
         // 3. Вывод реверсивного числа и суммы его цифр.
         System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр.");
-        int number = 1234;
+        int srcNumber = 1234;
         int sum = 0;
-        while(number != 0) {
-            int revNumber = number % 10;
-            sum += revNumber;
-            number /= 10;
-            System.out.print(revNumber);
+        while(srcNumber != 0) {
+            int digit = srcNumber % 10;
+            sum += digit;
+            srcNumber /= 10;
+            System.out.print(digit);
         }
         System.out.println("\nСумма чисел: " + sum);
 
         // 4. Вывод чисел на консоль в несколько строк
         System.out.println("\n4. Вывод чисел на консоль в несколько строк.");
-        int currNum = -1;
-        for(int i = 1; i <= 3; i++) {
-            for(int j = 1; j <= 5; j++) {
-                currNum += 2;
-                if(currNum > 24) {
-                    System.out.print("00 ");
+        int count = 1;
+        for(int i = 1; i < 30; i += 2) {
+            if(count % 5 != 0) {
+                if(i < 24) {
+                    System.out.printf("%3d", i);
                 } else {
-                    System.out.print(new DecimalFormat("00").format(currNum) + " ");
+                    System.out.printf("%3d", 0);
+                }
+            } else {
+                if(i < 24) {
+                System.out.printf("%3d%n", i);
+                } else {
+                    System.out.printf("%3d%n", 0);
                 }
             }
-            System.out.println();
+            count++;
         }
 
 
         // 5. Проверка количества единиц на четность
-        System.out.println("\n\n5. Проверка количества единиц на четность");
-        number = 3141591;
-        int onesSum = 0;
-        while(number != 0) {
-            currNum = number % 10;
-            if(currNum == 1) {
-                onesSum += currNum;
+        System.out.println("\n5. Проверка количества единиц на четность");
+        srcNumber = 3141591;
+        int sumOnes = 0;
+        while(srcNumber != 0) {
+            int digit = srcNumber % 10;
+            if(digit == 1) {
+                sumOnes += digit;
             }
-            number/= 10;
+            srcNumber /= 10;
         }
-        if(onesSum % 2 == 0) {
-            System.out.println("Колличество едениц чётное число " + onesSum);
+        if(sumOnes % 2 == 0) {
+            System.out.println("Колличество едениц чётное число " + sumOnes);
         } else {
-            System.out.println("Колличество едениц не чётное число " + onesSum);
+            System.out.println("Колличество едениц не чётное число " + sumOnes);
         }
 
         // 6. Отображение фигур в консоли.
@@ -114,9 +115,10 @@ public class CyclesTheme {
             j = 0;
             System.out.println();
         }
-        // Я не понял как третью фигуру сделать одним блоком вложенных циклов... Сделал двумя.
+
         i = 0;
         j = 1;
+        count = 1;
         do {
             do {
                 System.out.print("$");
@@ -124,61 +126,56 @@ public class CyclesTheme {
             } while(j <= i);
             System.out.println();
             i++;
-            j = 0;
-        } while(i < 3);
-
-        i = 1;
-        do {
-            do {
-                System.out.print("$");
-                j++;
-            } while(j <= i);
-            System.out.println();
-            i--;
-            j = 1;
-        } while(i >= 0);
+            if(i < 3) {
+                j = 0;
+            } else {
+                count *= 2;
+                j = count;
+            }
+        } while(i < 5);
 
         // 7. Отображение ASCII-символов.
         System.out.println("\n7. Отображение ASCII-символов.");
         System.out.println("Dec Char");
         for(i = 0; i <= 127; i++) {
-            char simbol = (char)i;
-            System.out.println(new DecimalFormat("000").format(i) + "  " + simbol);
+            char simbol = (char) i;
+            System.out.printf("%3d", i);
+            System.out.println("  " + simbol);
         }
 
         // 8. Проверка, является ли число палиндромом.
         System.out.println("\n8. Проверка, является ли число палиндромом.");
-        number = 12321;
-        int copyNumber = number;
-        int revNumber = 0;
-        while(number != 0) {
-            revNumber *= 10;
-            revNumber += number % 10;
-            number /= 10;
+        srcNumber = 12321;
+        int copySrcNumber = srcNumber;
+        int digit = 0;
+        while(srcNumber != 0) {
+            digit *= 10;
+            digit += srcNumber % 10;
+            srcNumber /= 10;
         }
-        if(copyNumber == revNumber) {
-            System.out.println("Число " + copyNumber + " является палиндромом.");
+        if(copySrcNumber == digit) {
+            System.out.println("Число " + copySrcNumber + " является палиндромом.");
         } else {
-            System.out.println("Число " + copyNumber + " не является палиндромом.");
+            System.out.println("Число " + copySrcNumber + " не является палиндромом.");
         }
 
         // 9. Определение, является ли число счастливым.
         /* Не могу придумать как выделить и записать каждую цифру отдельно.
         Смог просто проверить на "счастливость"*/
         System.out.println("\n9. Определение, является ли число счастливым.");
-        number = 456375;
-        int abc = number / 1000;
+        srcNumber = 456375;
+        int abc = srcNumber / 1000;
         int sumAbc = 0;
         while(abc != 0) {
-            currNum = abc % 10;
-            sumAbc += currNum;
+            digit = abc % 10;
+            sumAbc += digit;
             abc /= 10;
         }
         int sumDef = 0;
         for(i = 0; i < 3; i++) {
-            currNum = number % 10;
-            sumDef += currNum;
-            number /= 10;
+            digit = srcNumber % 10;
+            sumDef += digit;
+            srcNumber /= 10;
         }
         if(sumAbc == sumDef) {
             System.out.println("Сумма первых трёх чисел равно " + sumAbc + " Сумма последних " + sumDef + " Число счастливое.");
@@ -193,7 +190,7 @@ public class CyclesTheme {
                 if(j * i == 1) {
                     System.out.print("   ");
                 } else {
-                System.out.print(new DecimalFormat("00").format(j * i) + " ");
+                System.out.printf("%3d", (j * i));
                 }
             }
             System.out.println();
