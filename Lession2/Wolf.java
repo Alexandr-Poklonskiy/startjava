cab249e492ff09ac28259d607a7377ea5701de02
Alexandr-Poklonskiy <poklonskiy.a@gmail.com>
2022-04-14 01:05:30 +0300

    Модифицировал Wolf и WolfTest в соответствии с темой Инкапсуляция

--- Lession2/Wolf.java
+++ Lession2/Wolf.java
@@ -1,36 +1,80 @@
 public class Wolf {
 
-    String name;
-    String sex;
-    String color;
-    byte age;
-    float weight;
+    private String name;
+    private String sex;
+    private String color;
+    private byte age;
+    private float weight;
 
     public Wolf(String name) {
         this.name = name;
     }
 
-    void jump() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public  byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст.");
+        } else {
+        this.age = age;
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void jump() {
         System.out.println("Прыгает.");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Бежит.");
     }
 
-    void eat() {
+    public void eat() {
         System.out.println("Ест.");
     }
 
-    void bite() {
+    public void bite() {
         System.out.println("Кусает за бочок.");
     }
 
-    void missed() {
+    public void missed() {
         System.out.println(name + " промахнулся.");
     }
     
-    void say() {
+    public void say() {
         System.out.println("Мы с тобой одной крови!");
     }
 }
--- Lession2/WolfTest.java
+++ Lession2/WolfTest.java
@@ -1,13 +1,16 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf alfa = new Wolf("Акелла");
+        alfa.setSex("кабель");
+        alfa.setColor("серый");
+        alfa.setAge((byte) 8);
+        alfa.setWeight((float) 62.8);
 
-        alfa.age = 10;
-        alfa.sex = "кабель";
-        alfa.weight = 62.8f;
-        alfa.color = "серый";
-
-        System.out.println("Кличка " + alfa.name + "\nПол " + alfa.sex + "\nВозраст " + alfa.age + "\nВес " + alfa.weight + "\nЦвет " + alfa.color);
+        System.out.println("Кличка: " + alfa.getName());
+        System.out.println("Пол: " + alfa.getSex());
+        System.out.println("Окрас: " + alfa.getColor());
+        System.out.println("Возраст: " + alfa.getAge());
+        System.out.println("Вес: " + alfa.getWeight());
 
         alfa.say();
         alfa.run();
