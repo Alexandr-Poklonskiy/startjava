07c85a156c7072ebf815af39fb09161ad7a2349f
Alexandr-Poklonskiy <poklonskiy.a@gmail.com>
2022-05-01 13:02:04 +0300

    Сделал первую часть Игры

--- src/com/startjava/lession2_3_4/game/GuessNumber.java
+++ src/com/startjava/lession2_3_4/game/GuessNumber.java
@@ -7,6 +7,7 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
     private int secretNumber;
+    private int index;
     private Scanner scan = new Scanner(System.in);
 
     public GuessNumber(Player player1, Player player2) {
@@ -17,29 +18,42 @@ public class GuessNumber {
     void start() {
         Random random = new Random();
         secretNumber = random.nextInt(100) + 1;
-        System.out.println("Компьютер загадал число от 1 до 100. Отгадайте!");
-        while(true) {
+        System.out.println("Компьютер загадал число от 1 до 100. Отгадайте! У вас по 10 попыток.");
+        for(index = 0; index < 10;  index++) {
             System.out.println(player1.getName() + " Твой ход!");
-            player1.setNumber(scan.nextInt());
-            if(player1.getNumber() > secretNumber) {
+            player1.setNumbers(scan.nextInt(), index);
+            if(player1.getNumber(index) > secretNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
-            } else if(player1.getNumber() < secretNumber) {
+            } else if(player1.getNumber(index) < secretNumber) {
                  System.out.println("Данное число меньше того, что загадал компьютер");
             } else {
-                System.out.println(player1.getName() + " ты великолепен! Загаданное число " + secretNumber);
-                break;
+                player1.win(secretNumber, index);
+                /* Из-за того что, если выигрывает первый игрок, у второго игрока количество введённых чисел меньше на 1,
+                    метод, который записывает результаты ввода для второго игрока, я разместил в двух местах,
+                    в зависимости от того кто выиграл*/
+                player2.inNumbers(index - 1);
+            }
+            if(index == 9) {
+                System.out.println("У игрока " + player1.getName() + " закончились попытки.");
             }
 
+
             System.out.println(player2.getName() + " Твой ход!");
-            player2.setNumber(scan.nextInt());
-            if(player2.getNumber() > secretNumber) {
+            player2.setNumbers(scan.nextInt(), index);
+            if(player2.getNumber(index) > secretNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
-            } else if(player2.getNumber() < secretNumber) {
+            } else if(player2.getNumber(index) < secretNumber) {
                 System.out.println("Данное число меньше того, что загадал компьютер");
             } else {
-                System.out.println(player2.getName() + " ты великолепен! Загаданное число " + secretNumber);
+                player2.win(secretNumber, index);
+                player2.inNumbers(index);
                 break;
             }
         }
+        player1.inNumbers(index);
+        player1.showResult();
+        player2.showResult();
+        player1.resetNumbers(index);
+        player2.resetNumbers(index);
     }
 }
--- src/com/startjava/lession2_3_4/game/Player.java
+++ src/com/startjava/lession2_3_4/game/Player.java
@@ -1,8 +1,11 @@
 package com.startjava.lession2_3_4.game;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
-    private int number;
+    private int[] numbers = new int[10];
+    private int [] copyNumbers;
 
     public Player(String name) {
         this.name = name;
@@ -12,11 +15,27 @@ public class Player {
         return name;
     }
 
-    public int getNumber() {
-        return number;
+    public int getNumber(int i) {
+        return numbers[i];
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public void setNumbers(int number, int i) {
+        numbers[i] = number;
+    }
+
+    public void resetNumbers(int index) {
+        Arrays.fill(numbers, 0, index + 1, 0);
+    }
+
+    public void win(int secretNumber, int index) {
+        System.out.println(name + "угадал число " + secretNumber + " с " + (index + 1) + " попытки!");
+    }
+
+    public void inNumbers(int index) {
+        copyNumbers = Arrays.copyOf(numbers, index + 1);
+    }
+
+    public  void showResult() {
+        System.out.println("Игрок " + name + " ввёл числа " + Arrays.toString(copyNumbers));
     }
 }
