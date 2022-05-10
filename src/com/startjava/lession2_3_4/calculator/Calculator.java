5076aa18ad4406b3be2a46be12004a0c830c0b2b
Alexandr-Poklonskiy <poklonskiy.a@gmail.com>
2022-05-10 23:52:22 +0300

    Сделал калькулятор со *

--- src/com/startjava/lession2_3_4/calculator/Calculator.java
+++ src/com/startjava/lession2_3_4/calculator/Calculator.java
@@ -1,27 +1,32 @@
 package com.startjava.lession2_3_4.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
-    public int calculate(String mathExpression) {
-        String[] partsExpression = mathExpression.split(" ");
-        int x = Integer.parseInt(partsExpression[0]);
-        int y = Integer.parseInt(partsExpression[2]);
-        char sign = partsExpression[1].charAt(0);
-        switch (sign) {
-            case '+' :
-                return Math.addExact(x, y);
-            case '-' :
-                return Math.subtractExact(x, y);
-            case '*' :
-                return Math.multiplyExact(x, y);
-            case '/' :
-                return Math.floorDiv(x, y);
-            case '%' :
-                return x % y;
-            case '^' :
-                return (int) Math.pow(x, y);
-            default :
-                return 0;
-        }
+    public static int calculate() {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int y;
+        String[] simbolsMathExp;
+        do {
+            System.out.print("Введите математическое выражение: ");
+            simbolsMathExp = scan.nextLine().split(" ");
+            x = Integer.parseInt(simbolsMathExp[0]);
+            y = Integer.parseInt(simbolsMathExp[2]);
+            if (x < 0 || y < 0) {
+                System.out.println("Принимаются целые положительные числа");
+            }
+        } while (x < 0 || y < 0 );
+        char sign = simbolsMathExp[1].charAt(0);
+        return switch(sign) {
+            case '+' -> Math.addExact(x, y);
+            case '-' -> Math.subtractExact(x, y);
+            case '*' -> Math.multiplyExact(x, y);
+            case '/' -> Math.floorDiv(x, y);
+            case '%' -> x % y;
+            case '^' -> (int) Math.pow(x, y);
+            default -> 0;
+        };
     }
 }
--- src/com/startjava/lession2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lession2_3_4/calculator/CalculatorTest.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
         Scanner scan = new Scanner(System.in);
         String answer;
         do {
-            System.out.print("Введите математическое выражение: ");
-            System.out.println("Ответ: " + calc.calculate(scan.nextLine()));
+            System.out.println("Ответ: " + Calculator.calculate());
             do {
                 System.out.print("Хотите продолжить вычисления? [yes/no]: ");
                 answer = scan.nextLine();
